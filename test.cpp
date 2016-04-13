03a0328f15e83f7de14819c9a28dcad0367630f5
Anastasia Antonova <nany.antonova@yandex.ru>
2016-04-13 09:17:47 +0300

    error with make_pair

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <bitset>
 #include <vector>
+#include <map>
 #include <string>
 #include <seqan/seq_io.h>
 #include <chrono>
@@ -8,8 +9,6 @@
 struct bitread{
 	static const size_t maxlen = 640;
 	static const std::bitset<maxlen> * init_masks () {
-		//int const maxlen = 3;
-		//std::bitset<maxlen> mask[maxlen];
 		static std::bitset<maxlen> mask[maxlen];
 		mask[0].set();
 		for (size_t i=1; i<maxlen; i++){
@@ -36,20 +35,14 @@ struct bitread{
 			oddbit[i] = tmp[1];
 		}
 	}
+	
+	bitread() {}
 
-	int half_hamming_shift(const bitread &s){
-        	size_t mlen = std::min<size_t>(s.len, len);
-        	std::bitset<maxlen> res = ((s.evenbit ^ evenbit) | (s.oddbit ^ oddbit)) << (maxlen-mlen);
-        	return res.count();
-	}
-
-	int half_hamming_mask(const bitread &s){
+	int dist_mask(const bitread &s){
                 size_t mlen = std::min<size_t>(s.len, len);
                 std::bitset<maxlen> res = ((s.evenbit ^ evenbit) | (s.oddbit ^ oddbit)) & bitread::mask(maxlen-mlen);
                 return res.count();
         }
-	
-
 };
 
 template<typename Ts1, typename Ts2>
@@ -59,12 +52,34 @@ int old_half_hamming(const Ts1 &s1, const Ts2 &s2) {
 
     int res = 0;
     for (size_t i = 0; i < len; ++i) {
-        //res += (s1[i] == s2[i]) ? 0 : 1;
     	if (s1[i] != s2 [i])
 		res++;
 	}
     return res;
 }
+
+struct AllShifts{
+        std::map<bitread, int> shifts;
+        static const size_t min_overlap_len = 300;
+        AllShifts (bitread b){
+		bitread tmp;
+		tmp.evenbit = b.evenbit;
+		tmp.oddbit = b.oddbit;
+		
+                int max_shift = b.len - min_overlap_len;
+                for (size_t i=1; i<max_shift; i++){
+                        tmp.evenbit = tmp.evenbit >> 1;
+			tmp.oddbit = tmp.oddbit >> 1;
+                        shifts.insert (std::make_pair(b,i));
+                }
+                tmp = b;
+                for (size_t i=1; i<max_shift; i++){
+                        tmp.evenbit = tmp.evenbit << 1;
+                        tmp.oddbit = tmp.oddbit << 1;
+                        shifts.insert (make_pair(b,-i));
+                }
+        }
+};
 
 using std::vector;
 using seqan::Dna5String;
@@ -90,12 +105,12 @@ int main ()
 	for (const auto &_ : reads) {
 		bits.push_back(bitread(_));
 	}
-	int dist1 = 0, dist2 = 0, dist3 = 0;
+	int dist1 = 0, dist3 = 0;
 	auto start_time = std::chrono::steady_clock::now(); // new
 	for (size_t i = 0; i < bits.size(); i++) {
 		for (size_t j = 0/*i*/; j < bits.size(); j++){
 			//std::cout << bits[i].half_hamming (bits[j]) << ' ';
-			dist1+=(bits[i].half_hamming_shift (bits[j]));
+			dist1+=(bits[i].dist_mask (bits[j]));
 		}
 		
 	}
@@ -103,31 +118,15 @@ int main ()
 	auto t = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time - start_time);
 	std::cout << t.count() << std::endl; 
 	
-	auto start_time1 = std::chrono::steady_clock::now(); // new mask
-        for (size_t i = 0; i < bits.size(); i++) {
-                for (size_t j = 0/*i*/; j < bits.size(); j++){
-                        //std::cout << bits[i].half_hamming (bits[j]) << ' ';
-                        dist2+=(bits[i].half_hamming_mask (bits[j]));
-                }
-
-        }
-        auto end_time1 = std::chrono::steady_clock::now();
-        auto t1 = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time1 - start_time1);
-        std::cout << t1.count() << std::endl;
-
-	auto start_time2 = std::chrono::steady_clock::now(); // old
+        auto start_time2 = std::chrono::steady_clock::now(); // old
         for (size_t i = 0; i < bits.size(); i++) {
                 for (size_t j = 0/*i*/; j < bits.size(); j++){
                         //std::cout << old_half_hamming (reads[i],reads[j]) << ' ';
         		dist3+=(old_half_hamming (reads[i],reads[j])); 
-	       }
-                
-        }
-        auto end_time2 = std::chrono::steady_clock::now();
+	  	}
+         }
+	auto end_time2 = std::chrono::steady_clock::now();
         auto t2 = std::chrono::duration_cast<std::chrono::nanoseconds>(end_time2 - start_time2);
-	std::cout << t2.count() << std::endl;
-	if (dist1 == dist3)
-		 std::cout << "ok" << reads.size() << std::endl;
-	return 0;
+        std::cout << t2.count() << std::endl;
 
-}
+} 
